f8dbc8d0b01f720a1afe78b25e840b7211e4dcf3
dfangs <dfanggohans@gmail.com>
2024-07-05 01:04:14 -0700

    Reformat TS files using `biome`

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
+import * as fs from "node:fs/promises";
+import type chroma from "chroma-js";
 import { DARK_FANGS } from "./themes/fangs.js";
-import chroma from "chroma-js";
-import * as fs from "fs/promises";
 
 function isColor(value: unknown): value is chroma.Color {
-	return (
-		typeof value === "object" &&
-		value !== null &&
-		"hex" in value &&
-		typeof value.hex === "function"
-	);
+  return (
+    typeof value === "object" &&
+    value !== null &&
+    "hex" in value &&
+    typeof value.hex === "function"
+  );
 }
 
 const THEMES_DIR = "./themes";
@@ -16,10 +16,10 @@ const THEMES_DIR = "./themes";
 await fs.mkdir(THEMES_DIR, { recursive: true });
 
 await Promise.all(
-	[DARK_FANGS].map((theme) =>
-		fs.writeFile(
-			`${THEMES_DIR}/${theme.name.toLowerCase().replace(" ", "-")}.json`,
-			JSON.stringify(theme, (_, value) => (isColor(value) ? value.hex() : value), 2),
-		),
-	),
+  [DARK_FANGS].map((theme) =>
+    fs.writeFile(
+      `${THEMES_DIR}/${theme.name.toLowerCase().replace(" ", "-")}.json`,
+      JSON.stringify(theme, (_, value) => (isColor(value) ? value.hex() : value), 2),
+    ),
+  ),
 );
--- src/primer.ts
+++ src/primer.ts
@@ -6,8 +6,8 @@ const COLOR_SCALE_LENGTH = 10;
 // Helper type for n-length tuple
 // Reference: https://stackoverflow.com/a/65914848
 type Tuple<T, N extends number, A extends Array<T> = []> = A extends { length: N }
-	? A
-	: Tuple<T, N, [...A, T]>;
+  ? A
+  : Tuple<T, N, [...A, T]>;
 
 // Type aliases
 type ColorRange = Tuple<chroma.Color, 2>;
@@ -17,120 +17,120 @@ type ColorScale = Tuple<chroma.Color, typeof COLOR_SCALE_LENGTH>;
  * Arguments to `Primer.fromScale` factory method
  */
 interface PrimerScaleArgs {
-	black: chroma.Color;
-	white: chroma.Color;
-	gray: ColorScale;
-	blue: ColorScale;
-	green: ColorScale;
-	yellow: ColorScale;
-	orange: ColorScale;
-	red: ColorScale;
-	purple: ColorScale;
-	pink: ColorScale;
+  black: chroma.Color;
+  white: chroma.Color;
+  gray: ColorScale;
+  blue: ColorScale;
+  green: ColorScale;
+  yellow: ColorScale;
+  orange: ColorScale;
+  red: ColorScale;
+  purple: ColorScale;
+  pink: ColorScale;
 }
 
 /**
  * Arguments to `Primer.fromRange` factory method.
  */
 interface PrimerRangeArgs {
-	black: chroma.Color;
-	white: chroma.Color;
-	gray: ColorRange;
-	blue: ColorRange;
-	green: ColorRange;
-	yellow: ColorRange;
-	orange: ColorRange;
-	red: ColorRange;
-	purple: ColorRange;
-	pink: ColorRange;
+  black: chroma.Color;
+  white: chroma.Color;
+  gray: ColorRange;
+  blue: ColorRange;
+  green: ColorRange;
+  yellow: ColorRange;
+  orange: ColorRange;
+  red: ColorRange;
+  purple: ColorRange;
+  pink: ColorRange;
 }
 
 /**
  * Represents a color primer.
  */
 export class Primer {
-	constructor(
-		public readonly black: chroma.Color,
-		public readonly white: chroma.Color,
-		public readonly gray: ColorScale,
-		public readonly blue: ColorScale,
-		public readonly green: ColorScale,
-		public readonly yellow: ColorScale,
-		public readonly orange: ColorScale,
-		public readonly red: ColorScale,
-		public readonly purple: ColorScale,
-		public readonly pink: ColorScale,
-	) {}
+  constructor(
+    public readonly black: chroma.Color,
+    public readonly white: chroma.Color,
+    public readonly gray: ColorScale,
+    public readonly blue: ColorScale,
+    public readonly green: ColorScale,
+    public readonly yellow: ColorScale,
+    public readonly orange: ColorScale,
+    public readonly red: ColorScale,
+    public readonly purple: ColorScale,
+    public readonly pink: ColorScale,
+  ) {}
 
-	/**
-	 * Creates a color primer based on the given color scales.
-	 *
-	 * @param args primer arguments
-	 * @returns primer
-	 */
-	public static fromScale(args: PrimerScaleArgs): Primer {
-		return new Primer(
-			args.black,
-			args.white,
-			args.gray,
-			args.blue,
-			args.green,
-			args.yellow,
-			args.orange,
-			args.red,
-			args.purple,
-			args.pink,
-		);
-	}
+  /**
+   * Creates a color primer based on the given color scales.
+   *
+   * @param args primer arguments
+   * @returns primer
+   */
+  public static fromScale(args: PrimerScaleArgs): Primer {
+    return new Primer(
+      args.black,
+      args.white,
+      args.gray,
+      args.blue,
+      args.green,
+      args.yellow,
+      args.orange,
+      args.red,
+      args.purple,
+      args.pink,
+    );
+  }
 
-	/**
-	 * Creates a color primer based on color ranges instead of color scales.
-	 *
-	 * Note:
-	 * This is an experimental attempt at programmatically generating
-	 * the theme color palette, but so far this has not worked well.
-	 *
-	 * @param args primer arguments
-	 * @returns primer
-	 */
-	public static fromRange(args: PrimerRangeArgs): Primer {
-		return new Primer(
-			args.black,
-			args.white,
-			generateColorScale(args.gray),
-			generateColorScale(args.blue),
-			generateColorScale(args.green),
-			generateColorScale(args.yellow),
-			generateColorScale(args.orange),
-			generateColorScale(args.red),
-			generateColorScale(args.purple),
-			generateColorScale(args.pink),
-		);
-	}
+  /**
+   * Creates a color primer based on color ranges instead of color scales.
+   *
+   * Note:
+   * This is an experimental attempt at programmatically generating
+   * the theme color palette, but so far this has not worked well.
+   *
+   * @param args primer arguments
+   * @returns primer
+   */
+  public static fromRange(args: PrimerRangeArgs): Primer {
+    return new Primer(
+      args.black,
+      args.white,
+      generateColorScale(args.gray),
+      generateColorScale(args.blue),
+      generateColorScale(args.green),
+      generateColorScale(args.yellow),
+      generateColorScale(args.orange),
+      generateColorScale(args.red),
+      generateColorScale(args.purple),
+      generateColorScale(args.pink),
+    );
+  }
 
-	/**
-	 * Reverses the color scheme of this primer between dark <-> light.
-	 *
-	 * @returns reversed primer
-	 */
-	public reverse(): Primer {
-		function reverseScale(scale: ColorScale): ColorScale {
-			return [...scale].reverse() as ColorScale;
-		}
+  /**
+   * Reverses the color scheme of this primer between dark <-> light.
+   *
+   * @returns reversed primer
+   */
+  public reverse(): Primer {
+    function reverseScale(scale: ColorScale): ColorScale {
+      return [...scale].reverse() as ColorScale;
+    }
 
-		return new Primer(
-			this.white,
-			this.black,
-			reverseScale(this.gray),
-			reverseScale(this.blue),
-			reverseScale(this.green),
-			reverseScale(this.yellow),
-			reverseScale(this.orange),
-			reverseScale(this.red),
-			reverseScale(this.purple),
-			reverseScale(this.pink),
-		);
-	}
+    return new Primer(
+      this.white,
+      this.black,
+      reverseScale(this.gray),
+      reverseScale(this.blue),
+      reverseScale(this.green),
+      reverseScale(this.yellow),
+      reverseScale(this.orange),
+      reverseScale(this.red),
+      reverseScale(this.purple),
+      reverseScale(this.pink),
+    );
+  }
 }
 
 /**
@@ -142,11 +142,11 @@ export class Primer {
  * @returns `length` equidistant color scale from the given color range
  */
 export function generateColorScale(
-	range: ColorRange,
-	length: number = COLOR_SCALE_LENGTH,
-	mode: chroma.InterpolationMode = "hsl",
+  range: ColorRange,
+  length: number = COLOR_SCALE_LENGTH,
+  mode: chroma.InterpolationMode = "hsl",
 ): ColorScale {
-	return chroma.scale(range).mode(mode).colors(length, null) as ColorScale;
+  return chroma.scale(range).mode(mode).colors(length, null) as ColorScale;
 }
 
 /**
@@ -156,5 +156,5 @@ export function generateColorScale(
  * @returns a color scale consisting of `chroma.Color`
  */
 export function toChromaArray(hexValues: Tuple<string, 10>): ColorScale {
-	return hexValues.map((hex) => chroma(hex)) as ColorScale;
+  return hexValues.map((hex) => chroma(hex)) as ColorScale;
 }
